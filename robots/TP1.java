127734c3a5a490321424b582ef6b0859823193be
Carlos Campos <31832440+czgcampos@users.noreply.github.com>
2019-02-24 18:48:04 +0000

    Add files via upload

--- robots/TP1.java
+++ robots/TP1.java
@@ -53,8 +53,9 @@ public class TP1 extends AdvancedRobot
 				go(18,18);
 			// Final prints
 			if(this.posX==18.0 && this.posY==18.0 && isRacing && this.turns>=3){
-				System.out.println("Distance Battle Done TP: " + this.distanceRoundDone);
-				System.out.println("Distance Battle Done OD: " + this.odometer.getRaceDistance());
+				System.out.println("Distance Battle Done: " + this.distanceBattleDone);
+				System.out.println("Distance Round Done: " + this.distanceRoundDone);
+				System.out.println("Distance Round Done Prof: " + this.odometer.getRaceDistance());
 				isRacing=false;
 			}
 		}
